68b7b5f43a80557310eca2f0abadcb8fcbf37ede
happystrawberry <inforhub@hotmail.com>
2019-11-08 13:28:23 +1100

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello SWU"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
